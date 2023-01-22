0ad4375fba3d306c14247f40d9bbd2ef13c967f9
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-01-22 23:26:42 +0000

    Add files via upload

---
+++ R_exercices/vector.R
@@ -0,0 +1,29 @@
+x<-c(1,2,3,4,5,6,7,8,9,10)
+x
+y = 1:10
+v = 1:1000
+z = 51:100
+z
+v
+m = seq(1,11)
+m
+!seq()
+?seq()
+m = seq(4, 40.5, 1.5)
+m
+b = c("Eddy","Teddy", "Veddy", "Seddy")
+b
+b = c("Eddy", 1, 2, 3)
+
+#Logical
+ C = c(TRUE, FALSE, TRUE, FALSE)
+Age = c(18,25,19,21,23)
+Gender = c("M","F","F","F","M")
+Covid = c("Yes", "No","Yes", "Yes","No")
+Student_record = data.frame(Age, Gender, Covid)
+Student_record
+str(Student_record)
+view(Student_record)
+?str()
+
+
